e2fa10f5bd862ed344439de44ad9c31a7e957ac3
MimiMc4 <miguelmarclave@gmail.com>
2026-02-22 17:36:38 +0100

    cambio en gestión de errores

--- internal/api/apierror/apierror.go
+++ internal/api/apierror/apierror.go
@@ -9,19 +9,51 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
-var ErrNotFound = errors.New("resource not found")
+var (
+	ErrNotFound      = errors.New("resource not found")
+	ErrAlreadyExists = errors.New("resource already exists")
+	ErrBadRequest    = errors.New("bad request")
+)
 
 // Función que detecta el tipo de error, y manda el código de error
 // correspondiente
 func DetectAndSendError(c *gin.Context, err error) {
 	var httpCode int
-	switch {
-	case errors.Is(err, ErrNotFound):
-		httpCode = http.StatusNotFound
-	default:
-		httpCode = http.StatusInternalServerError
+
+	// primero miramos errores de postgres
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		switch pgErr.Code {
+		case pgerrcode.UniqueViolation:
+			httpCode = http.StatusConflict
+
+		case pgerrcode.ForeignKeyViolation, pgerrcode.NotNullViolation,
+			pgerrcode.CheckViolation:
+
+			httpCode = http.StatusBadRequest
+		default:
+			// Cualquier otro error raro de BD
+			httpCode = http.StatusInternalServerError
+		}
+	} else {
+		switch {
+		case errors.Is(err, pgx.ErrNoRows) || errors.Is(err, ErrNotFound):
+			httpCode = http.StatusNotFound
+
+		case errors.Is(err, ErrAlreadyExists):
+			httpCode = http.StatusConflict
+
+		case errors.Is(err, ErrBadRequest):
+			httpCode = http.StatusBadRequest
+
+		default:
+			httpCode = http.StatusInternalServerError
+		}
 	}
 
 	SendError(c, httpCode, err)
