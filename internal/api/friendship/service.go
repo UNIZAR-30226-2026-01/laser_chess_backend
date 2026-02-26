d7d87dc5a25c7c0a97feb91f2ea5f458058bf2cd
Daniel Prieto <dprietotorrijos@gmail.com>
2026-02-26 13:01:07 +0100

    Consulta de base de datos terminada

--- internal/api/friendship/service.go
+++ internal/api/friendship/service.go
@@ -1,9 +1,13 @@
 package friendship
 
 import (
-	"context"
-
 	db "github.com/UNIZAR-30226-2026-01/laser_chess_backend/internal/db/sqlc"
 )
 
-type Frie
+type friendship struct{
+	queries *db.Queries
+}
+
+func NewService(q *db.Queries) *friendshipService {
+	return &friendshipService{queries: q}
+}
