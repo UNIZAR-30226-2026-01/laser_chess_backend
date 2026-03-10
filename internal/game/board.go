8df5f42790197b5542ce56b3597391421a205794
Daniel Prieto <dprietotorrijos@gmail.com>
2026-03-10 17:48:05 +0100

    Interfaz temporal, representación y propuesta de implementación usando interfaces y funciones para centralizar la lógica en el tablero. Función de testing primitiva y temporal del modelo

--- internal/game/board.go
+++ internal/game/board.go
@@ -1,5 +1,75 @@
 package game
 
+// DEFINICIÓN PENDIENTE DE REVISIÓN
 // Fichero que se encarga de definir las estructuras
 // de datos relacionadas con la lógica de una partida
 // de laser chess
+
+import "fmt"
+
+// ===================================== //
+//	PIECES								 //
+// ===================================== //
+
+// Interfaz general, toda pieza de tablero debe tener esto definido
+type BoardPiece interface {
+	canMoveTo(x int, y int) bool
+	canRotate(d rune) bool //temporal
+}
+
+// ============== KING ============== //
+
+type BoardPieceKing struct{
+	team rune //temporal
+}
+
+func (c *BoardPieceKing) canMoveTo(x int, y int) bool {
+	fmt.Printf("king - canMoveTo\n")
+	return true; //TODO
+}
+
+func (c *BoardPieceKing) canRotate(d rune) bool {
+	fmt.Printf("king - canRotate\n")
+	return false; //TODO
+}
+
+// ============== SHIELD ============== //
+
+type BoardPieceShield struct{
+	team rune //temporal
+	pointing int //temporal
+}
+
+func (c *BoardPieceShield) canMoveTo(x int, y int) bool {
+	fmt.Printf("shield - canMoveTo\n")
+	return true; //TODO
+}
+
+func (c *BoardPieceShield) canRotate(d rune) bool {
+	fmt.Printf("shield - canRotate\n")
+	return true; //TODO
+}
+
+// ===================================== //
+//	BOARD								 //
+// ===================================== //
+
+type Board struct {
+	cells [3][3]BoardPiece
+}
+
+func (b *Board) movePiece(x_from int, y_from int, x_to int, y_to int){
+	if(b.cells[x_from][y_from].canMoveTo(x_to, y_to)){
+		fmt.Printf("SAID TRUE\n")
+	}else{
+		fmt.Printf("SAID FALSE\n")
+	}
+}
+
+func (b *Board) rotatePiece(x_at int, y_at int, rot rune){
+	if(b.cells[x_at][y_at].canRotate(rot)){
+		fmt.Printf("SAID TRUE\n")
+	}else{
+		fmt.Printf("SAID FALSE\n")
+	}
+}
--- internal/game/engine.go
+++ internal/game/engine.go
@@ -3,3 +3,29 @@ package game
 // fichero que se encarga de la logica del juego de laser chess
 // tendra una rutina con la maquina de estados (o lo que sea)
 // principal del juego
+
+// ESPECIFICACIÓN
+
+/* --- TABLERO --- */
+
+/* - MoverPiezaDesde(old_x int, old_y int, new_x int, new_y int)
+* Desc:
+ */
+
+/* - RotarPieza(x int, y int, rotation char)
+* Desc:
+ */
+
+/* - PermutarPiezas(old_x int, old_y int, new_x int, new_y int)
+* Desc:
+ */
+
+/* --- Lógica de juego ---
+- Se mira si el movimiento es legal
+- Se ejecuta el movimiento
+- Se activa el laser
+- Se transforma el tablero
+- Se comprueba condición de victoria
+- Pasar turno
+*/
+
