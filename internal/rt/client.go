cb6c47ef80358175d228c3968be3c1423c2a8f0b
MSanJulian <msjulian46@gmail.com>
2026-03-12 13:39:25 +0100

    Clase room a medias, bastante planteada
    La clase room está a medio hacer, aunque no queda mucho, y la clase client está empezada muy poquito

--- internal/rt/client.go
+++ internal/rt/client.go
@@ -12,15 +12,31 @@ type Client struct {
 	Room      *Room
 }
 
+func (c *Client) InitClient(AccountID int64, Conn *websocket.Conn, 
+			 Room *Room) {
+	c.AccountID = AccountID
+	c.Conn = Conn
+	c.Send = make(chan interface{})
+	c.Room = Room
+}
+
 // lee mensajes del socket y los manda a la Room
 func (c *Client) ReadPump() {
 	// aqui ira un bucle for que escucha c.Conn.ReadJSON()
 	// y escribe en algun canal de Room
+	// for {
+	// 	v, err := c.Conn.ReadJSON()
+
+		
+	// }
 }
 
 // saca mensajes del canal c.Send y los escribe en el navegador
 func (c *Client) WritePump() {
 	// aquí ira un bucle for que escucha c.Send y hace c.Conn.WriteJSON()
+	// for {
+
+	// }
 }
 
 // cierra la conexion de un cliente
