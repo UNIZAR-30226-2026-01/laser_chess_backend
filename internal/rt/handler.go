845aa124328a2102c94359a5278c5fedf0737b80
MSanJulian <msjulian46@gmail.com>
2026-03-18 11:34:39 +0100

    Añadida funcion que pasa de http a websocket

--- internal/rt/handler.go
+++ internal/rt/handler.go
@@ -1,6 +1,18 @@
 package rt
 
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 // fichero que se encarga de upgradear conexiones HTTP a websoket
 // tendrá endpoints para partidas competitivas y privadas.
+
+func UpgradeConn(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error){
+	var upgrader *websocket.Upgrader
+	conn, err := upgrader.Upgrade(w, r, nil)
+	return conn, err
+}
 
 //TODO: todo
