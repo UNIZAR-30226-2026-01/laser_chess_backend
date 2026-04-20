8aff6712ec0a1859784c33e30c111674de4cc911
MSanJulian <msjulian46@gmail.com>
2026-04-20 11:23:00 +0200

    Actualizado endpoint para cambiar la contraseña

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -102,7 +102,7 @@ func SetupRouter(store *db.Store,
 		accountRoute.GET("/:id", accountHandler.GetOtherByID)
 		accountRoute.POST("/update", accountHandler.Update)
 		accountRoute.DELETE("/delete/", accountHandler.Delete)
-		accountRoute.PUT("/changePassword", accountHandler.ChangePassword)
+		accountRoute.PUT("/passwd", accountHandler.ChangePassword)
 	}
 
 	// Match routes
